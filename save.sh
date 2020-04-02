61faf89276a03e1f235f39f84d2cb89337263911
morphingsamurai <hirotakatech@gmail.com>
2020-04-02 13:43:01 +0200

    modificati nomi file errati

--- save.sh
+++ save.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 cp ~/.config/nvim/init.vim ./.config/nvim/init.vim
-cp ~/.bashrc ./bashrc
-cp ~/.tmux.conf ./tmux.conf
+cp ~/.bashrc ./.bashrc
+cp ~/.tmux.conf ./.tmux.conf
 git add .
 git commit -m "$1"
 git push
