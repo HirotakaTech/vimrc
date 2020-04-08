8f3a569e4e6893d4cad199c3e2a4e8de932c145a
morphingSamurai <hirotakatech@gmail.com>
2020-04-08 14:56:35 +0200

    Aggiunte configurazioni per chrome os

--- restore.sh
+++ restore.sh
@@ -2,5 +2,6 @@
 cp ./.config/nvim/init.vim ~/.config/nvim/init.vim
 cp ./.bashrc ~/.bashrc
 cp ./.tmux.conf ~/.tmux.conf
+cp ./.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 echo "File copiati"
 
--- save.sh
+++ save.sh
@@ -3,6 +3,7 @@ cp ~/.config/nvim/init.vim ./.config/nvim/init.vim
 cp ~/.bashrc ./.bashrc
 cp ~/.tmux.conf ./.tmux.conf
 cp ~/.config/alacritty/alacritty.yml ./.config/alacritty/alacritty.yml
+cp ~/.local/share/applications ./.local/share/applications
 git add .
 git commit -m "$1"
 git push
