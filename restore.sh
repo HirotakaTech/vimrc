06db3af7c8fe77968c2c207a06bbbdcd47f2ecab
morphingSamurai <hirotakatech@gmail.com>
2020-05-04 22:15:11 +0200

    aggiunta configurazione per coc

--- restore.sh
+++ restore.sh
@@ -3,8 +3,8 @@ sudo apt install -y fd-find fzf tree exuberant-ctags ripgrep
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/alacritty
 cp ./.config/nvim/init.vim ~/.config/nvim/init.vim
+cp ./.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
 cp ./.bashrc ~/.bashrc
 cp ./.tmux.conf ~/.tmux.conf
 cp ./.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 echo "File copiati"
-
